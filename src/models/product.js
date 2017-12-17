1b69b84e823985d3d9d6f5205cbf5f231abec00f
yangjunling <306950975@qq.com>
2017-12-17 11:29:18 +0800

    添加产品

--- src/models/product.js
+++ src/models/product.js
@@ -1,22 +1,59 @@
 'use strict';
 
-import db from '../lib/db';
+const db = require('./../lib/db.js');
+const estic = require('./../lib/elasticClient.js');
 
 const Product = {}
-const LOG = log4js.getLogger('file');
 
 Product.findOne = function (id, cb) {
-    // db.queryAsync('select * from t_product where id = ?', [id], function(err, results) {
-    //     if(results == null || results.length == 0) {
-    //         err = Error('empty')
-    //     }
-    //     cb(err, results[0])
-    // })
+    db.query('select * from t_product where id = ?', [id], function(err, results) {
+        if(results == null || results.length == 0) {
+            err = Error('empty')
+        }
+        cb(err, results)
+    })
 
     //Mock Scripts
-    const product = {"id": 1, "username" : "test", "password" : "test"}
-    cb(null, product)
+    // const product = {"id": 1, "username" : "test", "password" : "test"}
+    // cb(null, product)
 
+}
+
+Product.findList = function (cb) {
+    db.query('select * from t_product',function (err,results) {
+        if(results == null || results.length == 0){
+            err = Error('empty')
+        }
+        cb(err,results)
+    })
+}
+
+Product.insert = function (obj,cb) {
+    db.query('insert into t_product(name,description,price) values(?,?,?)', [obj.name,obj.description,obj.price], function(err, results) {
+        console.log(err)
+        if(results == null || results.length == 0) {
+            err = Error('empty')
+        }
+        cb(err, results)
+    })
+}
+
+Product.update = function (obj,cb) {
+    db.query('update t_product set name=?,description=?,price=?', [obj.name,obj.description,obj.price], function(err, results) {
+        if(results == null || results.length == 0) {
+            err = Error('empty')
+        }
+        cb(err, results)
+    })
+}
+
+Product.delete = function (id,cb) {
+    db.query('delete from t_product where id=?', [id], function(err, results) {
+        if(results == null || results.length == 0) {
+            err = Error('empty')
+        }
+        cb(err, results)
+    })
 }
 
 Product.verify = async function(username, password) {
@@ -42,4 +79,4 @@ Product.verify = async function(username, password) {
     }
 }
 
-export default Product;
+module.exports = Product;
